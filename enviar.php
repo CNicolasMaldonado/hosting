68170e8a57849fe65116e7764053c3414902b172
BuildTools <unconfigured@null.spigotmc.org>
2020-07-16 22:02:40 -0300

    micommit

---
+++ enviar.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nicolás Maldonado</title>
+    <link rel="shorcut icon" href="img/icono.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/lightbox.min.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header id="isTop">
+        <div class="header">
+            <img class="logo" src="img/logo.png">
+            <h1>Cantante y compositor: De Bragado hacia el MUNDO!</h1>
+            <div class="menu_bar">
+                <a href="#" class="bt_menu"><span class="icon-menu"></span> Menu</a>
+            </div>
+            <nav>
+                <ul>
+                    <a class="button" href="#home"><span class="icon-home2"></span> Home</a>
+                    <a class="button" href="#quiensoy"><span class="icon-star-empty"></span> Quién Soy</a>
+                    <a class="button" href="#servicios"><span class="icon-briefcase"></span> Servicios</a>
+                    <a class="button" href="#contacto"><span class="icon-envelop"></span> Contacto</a>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C192.71,130.75 269.47,-16.28 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgb(255, 240, 223);"></path></svg></div>
+
+    </header>
+
+    <div id="home" class="slideshow">
+        <ul class="slider">
+            <li>
+                <img src="img/slider/1.jpg" alt="">
+                <section class="caption">
+                    <h1>Lorem ipsum 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quisquam.</p>
+                </section>
+            </li>
+            <li>
+                <img src="img/slider/2.jpg" alt="">
+                <section class="caption">
+                    <h1>Lorem ipsum 2</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quisquam.</p>
+                </section>
+            </li>
+            <li>
+                <img src="img/slider/3.jpg" alt="">
+                <section class="caption">
+                    <h1>Lorem ipsum 3</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quisquam.</p>
+                </section>
+            </li>
+            <li>
+                <img src="img/slider/4.jpg" alt="">
+                <section class="caption">
+                    <h1>Lorem ipsum 4</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quisquam.</p>
+                </section>
+            </li>
+        </ul>
+
+        <ol class="pagination">
+            
+        </ol>
+    
+        <div class="left">
+            <span class="fa fa-chevron-left"></span>
+        </div>
+
+        <div class="right">
+            <span class="fa fa-chevron-right"></span>
+        </div>
+    </div>
+
+    <section id="quiensoy">
+        <h1>¿Quién Soy?</h1>
+        <a href="img/imagen.jpg" target="_blank">
+            <img src="img/imagen.jpg" class="imagen_acercaDe" alt="Imagen que quise poner yo">
+        </a>
+        <h3>¿De donde vengo? <a class="button2" href="#" id="abrir">Saber más</a></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem modi et officia blanditiis quos vel voluptatem repellat fugit facere maxime, distinctio alias, accusantium sunt aspernatur similique vitae inventore, magni error qui quia unde ad mollitia. Eaque, ratione sint dolores aliquid sed sapiente illo, natus quisquam harum quasi eos! Sunt distinctio, laudantium neque quae alias aliquam quis rerum. Nemo autem maiores, molestiae minus sit aperiam at qui repellendus, labore dicta, eaque odio! Iusto, illo optio tenetur sunt mollitia perspiciatis a illum, reiciendis unde quas commodi architecto pariatur error. Nulla natus sit provident quos architecto tempore veritatis debitis, repudiandae, consequuntur veniam et praesentium delectus mollitia quisquam! Autem aperiam voluptatum dolorem minus vero, ipsam repellendus, eos voluptatem ad illo enim amet dolor sint ullam sapiente aliquid hic laudantium officia temporibus corrupti ratione, earum architecto quasi perspiciatis! Nam tempora qui, provident ipsa unde nobis fugit, placeat labore quisquam maiores, cum amet id. Commodi ducimus, id facilis cumque mollitia, illo placeat fuga et corrupti nam voluptate sit rerum, molestias aliquam perspiciatis adipisci recusandae hic consequuntur ratione! Est illo consequuntur harum accusantium commodi laborum, earum ipsum obcaecati velit perferendis incidunt, recusandae dolor quasi quam fugiat et numquam neque repellat saepe porro optio accusamus eaque ipsam? Doloremque similique illum fugit natus iure doloribus, non vero accusamus deleniti quas quae deserunt vitae et aut eaque consequatur hic. Suscipit, culpa natus impedit nam ut dolorum error consectetur, facere molestias labore illum? Nesciunt aut ea repellendus quis ad reiciendis voluptates amet fuga sunt, recusandae quo deleniti. Explicabo sunt in reprehenderit vel, minus esse accusamus dolorem atque sint. Mollitia debitis doloremque nisi fugiat nulla fuga magnam sit. Aliquid quaerat labore ipsam exercitationem voluptatum placeat est aliquam eligendi voluptates neque suscipit, architecto quibusdam distinctio ab repellendus animi? Architecto, dicta dolores et voluptatibus neque sed natus vitae corporis, non id adipisci eos corrupti beatae tenetur voluptates sapiente incidunt a dolore fuga dignissimos cupiditate, similique ex perferendis? Tenetur aliquam assumenda quod nobis sed asperiores laudantium, tempora aperiam id fugiat ea, quibusdam et corporis fugit earum cupiditate, labore delectus aliquid sequi quae harum! Quis doloribus tempore eum esse vitae est iusto obcaecati dignissimos debitis nesciunt amet rem cupiditate asperiores officia numquam, a excepturi iure cum accusantium nobis doloremque! Debitis, delectus? Cumque, quam est omnis sed odit nihil maxime voluptas deleniti, ab pariatur ex tempora iste? Quis in cum asperiores deserunt, quibusdam doloremque, totam itaque, voluptates a illo accusamus necessitatibus suscipit? Atque ab, facere alias quo libero cumque. Doloribus atque tempore voluptatum expedita adipisci sunt neque pariatur id similique cupiditate sint sed illo harum iure qui accusantium quae, dignissimos eum fugiat officia natus obcaecati! Deserunt laboriosam consequuntur consequatur nostrum inventore iusto aperiam corporis ratione debitis harum tenetur accusantium cupiditate, illo eveniet ipsa alias reiciendis, cum fugiat.</p>
+        
+        <section class="grid">
+            <div class="contenedor">
+                <a class="grid_columna" href="img/grid/1.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/1.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/2.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/2.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/3.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/3.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/4.jpg" data-lightbox="roadtrip"> 
+                    <img src="img/grid/4.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/5.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/5.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/6.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/6.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/7.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/7.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/8.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/8.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/9.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/9.jpg">
+                </a> 
+                <a class="grid_columna" href="img/grid/10.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/10.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/11.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/11.jpg">
+                </a>
+                <a class="grid_columna" href="img/grid/12.jpg" data-lightbox="roadtrip">
+                    <img src="img/grid/12.jpg">
+                </a> 
+            </div>
+        </section>
+
+        <p align="right"><a class="button2" href="#isTop">Subir</a></p>
+    </section>
+
+    <section id="servicios">
+        <h1>Servicios</h1>
+        <h3>Ofrezco</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem modi et officia blanditiis quos vel voluptatem repellat fugit facere maxime, distinctio alias, accusantium sunt aspernatur similique vitae inventore, magni error qui quia unde ad mollitia. Eaque, ratione sint dolores aliquid sed sapiente illo, natus quisquam harum quasi eos! Sunt distinctio, laudantium neque quae alias aliquam quis rerum. Nemo autem maiores, molestiae minus sit aperiam at qui repellendus, labore dicta, eaque odio! Iusto, illo optio tenetur sunt mollitia perspiciatis a illum, reiciendis unde quas commodi architecto pariatur error. Nulla natus sit provident quos architecto tempore veritatis debitis, repudiandae, consequuntur veniam et praesentium delectus mollitia quisquam! Autem aperiam voluptatum dolorem minus vero, ipsam repellendus, eos voluptatem ad illo enim amet dolor sint ullam sapiente aliquid hic laudantium officia temporibus corrupti ratione, earum architecto quasi perspiciatis! Nam tempora qui, provident ipsa unde nobis fugit, placeat labore quisquam maiores, cum amet id. Commodi ducimus, id facilis cumque mollitia, illo placeat fuga et corrupti nam voluptate sit rerum, molestias aliquam perspiciatis adipisci recusandae hic consequuntur ratione! Est illo consequuntur harum accusantium commodi laborum, earum ipsum obcaecati velit perferendis incidunt, recusandae dolor quasi quam fugiat et numquam neque repellat saepe porro optio accusamus eaque ipsam? Doloremque similique illum fugit natus iure doloribus, non vero accusamus deleniti quas quae deserunt vitae et aut eaque consequatur hic. Suscipit, culpa natus impedit nam ut dolorum error consectetur, facere molestias labore illum? Nesciunt aut ea repellendus quis ad reiciendis voluptates amet fuga sunt, recusandae quo deleniti. Explicabo sunt in reprehenderit vel, minus esse accusamus dolorem atque sint. Mollitia debitis doloremque nisi fugiat nulla fuga magnam sit. Aliquid quaerat labore ipsam exercitationem voluptatum placeat est aliquam eligendi voluptates neque suscipit, architecto quibusdam distinctio ab repellendus animi? Architecto, dicta dolores et voluptatibus neque sed natus vitae corporis, non id adipisci eos corrupti beatae tenetur voluptates sapiente incidunt a dolore fuga dignissimos cupiditate, similique ex perferendis? Tenetur aliquam assumenda quod nobis sed asperiores laudantium, tempora aperiam id fugiat ea, quibusdam et corporis fugit earum cupiditate, labore delectus aliquid sequi quae harum! Quis doloribus tempore eum esse vitae est iusto obcaecati dignissimos debitis nesciunt amet rem cupiditate asperiores officia numquam, a excepturi iure cum accusantium nobis doloremque! Debitis, delectus? Cumque, quam est omnis sed odit nihil maxime voluptas deleniti, ab pariatur ex tempora iste? Quis in cum asperiores deserunt, quibusdam doloremque, totam itaque, voluptates a illo accusamus necessitatibus suscipit? Atque ab, facere alias quo libero cumque. Doloribus atque tempore voluptatum expedita adipisci sunt neque pariatur id similique cupiditate sint sed illo harum iure qui accusantium quae, dignissimos eum fugiat officia natus obcaecati! Deserunt laboriosam consequuntur consequatur nostrum inventore iusto aperiam corporis ratione debitis harum tenetur accusantium cupiditate, illo eveniet ipsa alias reiciendis, cum fugiat.</p>
+        <p align="right"><a class="button2" href="#isTop">Subir</a></p>
+    </section>
+
+    <section id="contacto">
+        <h1>Contacto</h1>
+        <p>Si deseas comunicarte vía mail puedes hacerlo dando click <a href="mailto:maldonado.nico@gmail.com?subject=Me%20encuentro%20interesado%20en%20sus%20servicios."><b>AQUI</b></a>.</p>
+        <p>Si deseas comunicarte de otra forma puedes llenar el siguiente formulario.</p>
+
+        <center>
+            <?php 
+            $myemail = 'maldonado.nico@gmail.com';
+            $name = $_POST['nombre'];
+            $email = $_POST['email'];
+            $message = $_POST['mensaje'];
+
+            $to = $myemail;
+            $email_subject = "Nuevo mensaje: $subject";
+            $email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+            $headers = "From: $email";
+
+            mail($to, $email_subject, $email_body, $headers);
+            echo "El mensaje se ha enviado correctamente";
+            ?>
+            <form method="post" action="enviar.php">
+                <input type="text" name="nombre" placeholder="NOMBRE">
+                <input type="email" name="email" placeholder="CORREO">
+                <br>
+                <textarea type="text" name="mensaje" placeholder="MENSAJE"></textarea>
+                <br>
+                <input type="submit" value="ENVIAR">
+            </form>
+        </center>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d2307.3173883077516!2d-60.47524843201314!3d-35.13068673116663!3m2!
+            1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x95beddc45c96b675%3A0x487a022d2d83690f!2sAmeghino%202891%2C%20Bragado%2C%20Buenos%20Aires!3m2
+            !1d-35.1307956!2d-60.474115399999995!5e0!3m2!1ses-419!2sar!4v1591288876355!5m2!1ses-419!2sar" width="100%" height="550" frameborder="0" 
+            style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <p align="right"><a class="button2" href="#isTop">Subir</a></p>
+    </section>
+
+    <div id="miModal" class="modal">
+        <div class="flex" id="flex">
+            <div class="modal-contain">
+                <div class="modal-header flex">
+                    <h2>Modal Agregado</h2>
+                    <span class="close" id="close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Texto de Prueba sin contenido de importancia, Ejemplo de Modal #1.</p>
+                </div>
+                <div class="mfooter">
+                    <h3>Footer del Modal.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>Realizado por NicolásMaldonado&copy;2020 todos los derechos reservados. <b><i>Mail: maldonado.nico@gmail.com</i></b></footer>
+
+    <script src="js/main.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/slider.js"></script>
+    <script src="js/lightbox-plus-jquery.min.js"></script>
+    <script src="js/lightbox.min.js"></script>
+</body>
+</html>
+
